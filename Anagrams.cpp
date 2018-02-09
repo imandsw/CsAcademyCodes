02f2486489cb1ba1b2166834c46b9aace948f073
imanefett <imanefettouch@gmail.com>
2018-02-09 20:32:42 +0000

    not working for rn

--- Anagrams.cpp
+++ Anagrams.cpp
@@ -12,18 +12,6 @@ using std::cin;
 bool checkAnagram(vector<string> v, int VectorSize){
   int i=0,j=0,k,compt=0;
   int test=0;
-  //vector<string>::iterator i=vec.begin();
-  //std::cout << (*i) << '\n';
-  //vector <int> :: iterator i;
-  //std::cout << v[1] << '\n';
-  //int length = (*it).length();
-  //for (it = v.begin(); it != v.end(); ++it)
-  //       cout << length << '\n';
-  //std::cout << v[0].size() << '\n';
-  //std::cout << v[0].size() << '\n';
-  //for ( i = 0; i < VectorSize; i++) {
-  //  std::cout << v[i].size() << '\n';
-  //}
   if (v[0].size()==v[1].size()){
     k=v[0].size();
     while (j<k){
@@ -43,7 +31,7 @@ bool checkAnagram(vector<string> v, int VectorSize){
     }
   if (compt==k) {
     return true;
-    std::cout << "cucu" << '\n'; // compt is exactly same as size means every letter is shown at least once in other string
+     // compt is exactly same as size means every letter is shown at least once in other string
   }
   else
   return false;
